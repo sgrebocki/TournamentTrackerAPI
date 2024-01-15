a7176ecda72f12de803bdd12c03e2f926d5a607f
sgrebock <szymon_grebocki@sevenet.pl>
2024-01-15 12:27:58 +0100

    Project initialization

---
+++ src/main/java/com/TournamentTracker/TournamentTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.TournamentTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TournamentTracker/TournamentTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TournamentTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
