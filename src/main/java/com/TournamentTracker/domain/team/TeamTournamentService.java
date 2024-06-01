e6cdee77114a1a362694655ec0cab47d6c517b89
sgrebock <szymon_grebocki@sevenet.pl>
2024-06-02 00:10:23 +0200

    signUp and signOut from Tournament methods

--- src/main/java/com/TournamentTracker/domain/team/TeamTournamentService.java
+++ src/main/java/com/TournamentTracker/domain/team/TeamTournamentService.java
@@ -3,6 +3,9 @@ package com.TournamentTracker.domain.team;
 import com.TournamentTracker.domain.team.model.TeamDto;
 import com.TournamentTracker.domain.team.model.TeamTournamentDto;
 
+import java.util.List;
+
 public interface TeamTournamentService {
-    TeamTournamentDto update(TeamDto teamDto, Long id);
+    TeamTournamentDto signUpForTournament(TeamDto teamDto, Long id);
+    TeamTournamentDto signOutFromTournament(TeamDto teamDto, Long id);
 }
