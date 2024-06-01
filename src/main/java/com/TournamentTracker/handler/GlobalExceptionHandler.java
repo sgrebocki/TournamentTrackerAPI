3c178136bcfdbeb64d02ef2b355464ae47ad1254
sgrebock <szymon_grebocki@sevenet.pl>
2024-06-01 13:50:57 +0200

    Added GlobalExceptionHandler

---
+++ src/main/java/com/TournamentTracker/handler/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.TournamentTracker.handler;
+
+import org.springframework.http.*;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return super.handleHttpMessageNotReadable(ex, headers, status, request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAll(Exception ex, WebRequest request) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).contentType(MediaType.APPLICATION_JSON).body(ex.getMessage());
+    }
+}
